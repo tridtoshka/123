38930fffe40d4124bc2f6dcc7d2bb73d493c82f0
tridtoshka <63330103+tridtoshka@users.noreply.github.com>
2020-04-09 21:50:47 +0700

    22
    333

---
+++ index.php
@@ -0,0 +1,107 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Документ без названия</title>
+<style type="text/css">
+body,td,th {
+	font-family: "Times New Roman", Times, serif;
+	font-size: 36px;
+	color: #000;
+	text-align: center;
+}
+body {
+	background-color: #999;
+}
+h1,h2,h3,h4,h5,h6 {
+	font-family: "Times New Roman", Times, serif;
+	font-weight: bold;
+}
+h1 {
+	font-size: 3cm;
+	color: #0F6;
+	text-align: center;
+}
+h2 {
+	font-size: 18px;
+	color: #06F;
+}
+#Prace p {
+	font-weight: bold;
+}
+</style>
+<link href="Tatel.css" rel="stylesheet" type="text/css" />
+<link href="jooble.css" rel="stylesheet" type="text/css" />
+<link href="Prace.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="Tatel">
+<h1>Онлайн курсы 3D Моделирования и 3D Скульптинга</h1>
+<p><img src="logotip 3Dtoska.png" width="320" height="425" align="top" /></p>
+<table width="1950" height="1362" border="10">
+<tr>
+      <th height="668" scope="col"><img src="Курсы.png" width="912" height="625" longdesc="Курсы.png" /></th>
+      <th scope="col"><img src="aae2f7604e6fdeceb65700082e7a228c.png" width="775" height="730" longdesc="aae2f7604e6fdeceb65700082e7a228c.png" /></th>
+  </tr>
+    <tr>
+      <th width="1043" height="668" scope="col"><p>3D моделирование - одно из самых перспективных и современных направлений развития компьютеных технологий. Если вы хотите модную и высокооплачиваемую профессию - эти курсы для ВАС.</p>
+        <p>Аниматоры от 50 тыс. рублей в месяц</p>
+        <p>Моделлеры от 30 тыс. рублей в месяц</p>
+      <p>Текстурщики от 25 тыс. рублей в месяцев</p></th>
+      <th width="913" scope="col"><p>Содержание программы курса:</p>
+        <p>1) Знакомство с программами 3D smax, Mudbox, Corel Drawe, Corel Photo-Paint, Adobe Afte Affrect;</p>
+        <p>2) Создание 3D-объектов реального мира;</p>
+        <p>3) Анимирование процессов и явлений реального мира из 3D-объектов;</p>
+      <p>4) Текстурирование 3D-объектов;</p></th>
+    </tr>
+</table>
+<p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <hr width="2500" size="10" id="Л" />
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <div id="jooble">
+    <p>Возможности после овладения курсом:</p>
+    <p>1) Подработка постояннная или разовая</p>
+    <p>2) Обучение 3d- моделированию</p>
+    <p>3) Создание рекламных роликов</p>
+    <p>4) Создание спецэффектов к видеороликам</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+  </div>
+<div>
+<div id="Prace">
+      <p>Стоимость обучения:</p>
+      <p>Общая стоимость обучения - 24 000 рублей</p>
+    <p>&nbsp;</p>
+      <p>Условия оплаты:</p>
+<p>Еженедельная - 1000 рублей</p>
+    <p>Первая неделя бесплатная    </p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <table width="1249" border="10">
+      <tr>
+        <th width="1221" height="511" bgcolor="#993333" scope="col"><p><strong>КОНТАКТНАЯ ИНФОРМАЦИЯ</strong></p>
+        <p><strong>электронная почта: pyotr.marchenko.1988@mail.ru</strong></p>
+        <p>Сотовый телефон: 8-960-963-19-65</p>
+        <p>Scape: 3Dтошка</p></th>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <table width="1268" border="10">
+      <tr>
+        <th width="1240" height="244" scope="col"><p>&copy;Все права защищены</p>
+        <p>2020 год г. Бийск</p></th>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+  </div>
+</div>
+</body>
+</html>
